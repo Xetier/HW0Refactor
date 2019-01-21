e815dc53354b418630c1292236d50a81d6eb16b4
Pieter Gleixner <pieter.gleixner@cignium.com>
2019-01-21 14:13:08 -0500

    LogIn refactor

---
+++ LogIn/LogIn/Hooks.cs
@@ -0,0 +1,32 @@
+﻿using Gauge.CSharp.Lib.Attribute;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogIn
+{
+    public class Hooks
+    {
+        public static IWebDriver _driver;
+
+        [BeforeSuite]
+        public void Initialize()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [AfterSuite]
+        public void AfterSuite()
+        {
+            _driver?.Quit();
+        }
+        public static IWebDriver WebDriver
+        {
+            get { return _driver; }
+        }
+    }
+}
