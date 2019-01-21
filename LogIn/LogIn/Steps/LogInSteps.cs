5362e6e82c6d646cf3c1f2d685d429556a00871b
Pieter Gleixner <pieter.gleixner@cignium.com>
2019-01-21 16:03:27 -0500

    Log in

--- LogIn/LogIn/Steps/LogInSteps.cs
+++ LogIn/LogIn/Steps/LogInSteps.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LogIn.Steps
@@ -18,5 +19,36 @@ namespace LogIn.Steps
             _driver.Navigate().GoToUrl(url);
             _driver.Manage().Window.Maximize();
         }
+
+        [Step("Click to button SingIn ")]
+        public void ClickToButtonSingIn()
+        {
+            _driver.FindElement(By.ClassName("login")).Click();
+        }
+
+        [Step("I wait <seconds> seconds")]
+        public void WaitFor(int seconds)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+        }
+
+        [Step("Fill email with <email>")]
+        public void FillEmail(string email)
+        {
+            _driver.FindElement(By.Id("email")).Clear();
+            _driver.FindElement(By.Id("email")).SendKeys(email);
+        }
+        [Step("Fill password with <pswrd>")]
+        public void FillPassword (string pswrd)
+        {
+            _driver.FindElement(By.Id("passwd")).Clear();
+            _driver.FindElement(By.Id("passwd")).SendKeys(pswrd);
+        }
+        [Step("Click Sign in button")]
+        public void ClickSignIn()
+        {
+            _driver.FindElement(By.Id("SubmitLogin")).Click();
+        }
+        //TODO: ASSERTS
     }
 }
